64a796f5d089c20b4e82fc05b3838ff93b02eb93
Hacene Sadoudi <sadoud_h@etna-alternance.net>
2023-02-07 17:43:19 +0100

    feat(sports): update sport module

--- GPE/sports/src/app.module.ts
+++ GPE/sports/src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { SportsModule } from './sports/sports.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URL),
-    UsersModule
+    SportsModule
   ],
   controllers: [AppController],
   providers: [AppService]
