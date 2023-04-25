7d17c8b8712f37117375a76ec6459f0a8de1367b
Hacene Sadoudi <sadoud_h@etna-alternance.net>
2023-04-25 17:23:00 +0200

    update login/signup pages

--- GPE/fitfrenzy/lib/controller/auth/auth_controller.dart
+++ GPE/fitfrenzy/lib/controller/auth/auth_controller.dart
@@ -14,27 +14,67 @@ class AuthController {
   Future<void> Login(
     String email,
     String password,
+    BuildContext context,
     Function(Response) callback,
   ) async {
-    var headers = {
-      'Content-Type': 'application/json',
-    };
+    var headers = {'Content-Type': 'application/json'};
 
-    final response =
-        await Dio().post('${dotenv.env['API_USER_URL']}/auth/login',
+    try {
+      var response = await Dio().post('${dotenv.env['API_USER_URL']}/auth/login',
             data: jsonEncode(<String, String>{
               'email': email,
               'password': password,
             }),
             options: Options(headers: headers));
-    callback(response);
+      callback(response);
+    } on DioError catch (e) {
+      if (e.response?.statusCode == 401) {
+         const snackBar = SnackBar(
+          content: Text("Email ou mot de passe incorrect. Veuillez réessayer."),
+          backgroundColor: Colors.red,
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      } else {
+        // handle other errors here
+        final snackBar = SnackBar(
+          content: Text(e.toString()),
+          backgroundColor: Colors.red,
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    }
+    
   }
 
-  Future<void> Register(User user, Function(Response) callback) async {
-    var header = {'Content-Type': 'application/json'};
-    final response = await Dio().post('$_url/users/signup',
-        queryParameters: header, data: user.toJson());
-
-    callback(response);
+  Future<void> Register(User user,BuildContext context, Function(Response) callback) async {
+    var dio = Dio();
+    try {
+      var response = await dio.post('$_url/users/signup',
+        data: user.toJson(),
+        options: Options(
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+        ));
+        callback(response);
+    } on DioError catch (e) {
+      if (e.response?.statusCode == 409) {
+        // handle the 409 Conflict error here
+        const snackBar = SnackBar(
+          content: Text("Désolé, cet e-mail est déjà utilisé. Veuillez en choisir un autre."),
+          backgroundColor: Colors.red,
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      } else {
+        // handle other errors here
+        final snackBar = SnackBar(
+          content: Text(e.toString()),
+          backgroundColor: Colors.red,
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+      
+    }
   }
 }
--- GPE/fitfrenzy/lib/views/auth/login.dart
+++ GPE/fitfrenzy/lib/views/auth/login.dart
@@ -17,6 +17,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   @override
@@ -37,126 +38,158 @@ class _LoginState extends State<Login> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(25.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(left: 10),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Nom d'utilisateur",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.all(8),
-                child: TextField(
-                  controller: _usernameController,
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20),
-                      ),
-                      borderSide: BorderSide(
-                        color: Colors.black,
-                        style: BorderStyle.solid,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center ,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Adresse e-mail",
+                      style: TextStyle(
+                        color: Colors.white,
                       ),
                     ),
-                    hintText:
-                        "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _",
-                    hintStyle: TextStyle(
-                      fontSize: 15,
-                      fontFamily: 'Open',
-                    ),
-                  ),
-                  keyboardType: TextInputType.text,
-                ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 10),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Mot de passe",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.all(8),
-                child: TextField(
-                  controller: _passwordController,
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20),
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  child: TextFormField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(2),
+                        ),
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          style: BorderStyle.solid,
+                        ),
                       ),
-                      borderSide: BorderSide(
-                        color: Colors.black,
-                        style: BorderStyle.solid,
+                      hintText:
+                          "Entrez votre adresse e-mail",
+                      hintStyle: TextStyle(
+                        fontSize: 15,
+                        fontFamily: 'Open',
+                      ),
+                      errorStyle: TextStyle(
+                        color: Colors.red, // Set the color of the error message
+                        fontSize: 14.0, // Set the font size of the error message
                       ),
                     ),
-                    hintText:
-                        "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _",
-                    hintStyle: TextStyle(
-                      fontSize: 15,
-                      fontFamily: 'Open',
+                    keyboardType: TextInputType.text,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Veuillez saisir votre adresse e-mail';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Mot de passe",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
                 ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.amber,
-                  fixedSize: const Size(170, 20),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25.0),
+                Container(
+                  margin: const EdgeInsets.all(8),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(2),
+                        ),
+                        borderSide: BorderSide(
+                          color: Colors.black,
+                          style: BorderStyle.solid,
+                        ),
+                      ),
+                      hintText:
+                          "Entrez votre mot de passe",
+                      hintStyle: TextStyle(
+                        fontSize: 15,
+                        fontFamily: 'Open',
+                      ),
+                      errorStyle: TextStyle(
+                        color: Colors.red, // Set the color of the error message
+                        fontSize: 14.0, // Set the font size of the error message
+                      ),
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Veuillez saisir un mot de passe';
+                      }
+                      return null;
+                    },
                   ),
                 ),
-                onPressed: () => AuthController().Login(
-                    _usernameController.text,
-                    _passwordController.text,
-                    (result) => {
-                          if (result.statusCode == 201)
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.amber,
+                    fixedSize: const Size(170, 20),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                  ),
+                  onPressed: () async {
+                    
+                    if (_formKey.currentState!.validate()) {
+
+                      await AuthController().Login(
+                        _emailController.text,
+                        _passwordController.text,
+                        context,
+                        (response) => {
+                          if (response.statusCode == 201)
                             {
-                              print('result.data: ${result.data['id']}'),
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
                                     builder: (context) =>
-                                        Home3(userId: result.data['id'])),
+                                        Home3(userId: response.data['id'])),
                               )
+                            } else{
+                              print("Error")
                             }
-                          else
-                            {print("Error")}
-                        }),
-                child: const Text(
-                  "Connexion",
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
+                        }
+                      );
+                    }
+                  },
+                
+                  child: const Text(
+                    "Connexion",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
