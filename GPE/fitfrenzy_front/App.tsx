7464591458b5c323524e7cc75ddca96d74c4efc8
Hacene Sadoudi <sadoud_h@etna-alternance.net>
2023-11-14 17:22:02 +0100

    create a custom splash screen

--- GPE/fitfrenzy_front/App.tsx
+++ GPE/fitfrenzy_front/App.tsx
@@ -1,12 +1,37 @@
 import { StyleSheet } from 'react-native';
 import Navigation from './src/navigation/Navigation';
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import RootStackScreens from './src/navigation/Navigation';
+import * as SplashScreen from 'expo-splash-screen';
+import Splash from './src/screens/Splash'
+import * as Font from 'expo-font';
 
-export default function App() {
+
+export default function App({navigation}: any) {
+  const [isSplashVisible, setSplashVisible] = useState(true);
+  
+  useEffect(() => {
+    //SplashScreen.preventAutoHideAsync();
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+        
+      });
+    };
+    loadFonts();
+   
+    // Simulate an async task, for example, loading data or performing authentication
+    setTimeout(() => {
+      setSplashVisible(false)
+      //SplashScreen.hideAsync();
+      //navigation.navigate('Home'); // Replace 'Home' with the screen you want to navigate to
+    }, 400); // Adjust the duration as needed
+  }, []);
+
   return (
       <NavigationContainer>
-        <Navigation />
+        {isSplashVisible ? <Splash /> : <RootStackScreens /> }
       </NavigationContainer>
   );
 }
