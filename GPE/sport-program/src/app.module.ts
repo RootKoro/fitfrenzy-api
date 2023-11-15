8c34bfc6ab972079b86312207872e553cc1b770d
Cyr Mathieu <gueye_d@etna-alternance.net>
2023-11-15 01:19:23 +0100

    added exercices and advices to the belonging modules

--- GPE/sport-program/src/app.module.ts
+++ GPE/sport-program/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ExerciceModule } from './exercices/exercice.module';
 import { ProgramModule } from './program/program.module';
 import { UserProgramModule } from './user-program/user-questions.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URL),
+    ExerciceModule,
     ProgramModule,
     UserProgramModule,
   ],
--- GPE/sports/src/app.module.ts
+++ GPE/sports/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdvicesModule } from './advices/advices.module';
 import { SportsModule } from './sports/sports.module';
 import { SwaggerModule } from '@nestjs/swagger';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URL),
+    AdvicesModule,
     SportsModule,
     SwaggerModule
   ],
