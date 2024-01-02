d64232ba01298a5031df70096141b2ad4b02d1d6
Hacene Sadoudi <sadoud_h@etna-alternance.net>
2024-01-02 17:11:24 +0100

    add userProfile method to api

--- GPE/fitfrenzy_front/src/api/auth.ts
+++ GPE/fitfrenzy_front/src/api/auth.ts
@@ -1,16 +1,22 @@
 import axios from 'axios'
 import { LoginResponse, RegistrationResponse, UserCredentials, UserData } from '../types/auth';
 import { useSelector } from 'react-redux';
-const BASE_URL = 'http://localhost:3000';
+import api from '.'
 
 export const login = async (userCredentials: UserCredentials) : Promise<LoginResponse | null> => {
-      const res = await axios.post<LoginResponse>(`${BASE_URL}/auth/login`, userCredentials);
+      const res = await api(3000).post<LoginResponse>(`/auth/login`, userCredentials);
       return res.data;
 };
 
 export const register = async (userData: UserData): Promise<RegistrationResponse | null> => {
-      const res = await axios.post<RegistrationResponse>(`${BASE_URL}/users/signup`, userData);
+      const res = await api(3000).post<RegistrationResponse>(`/users/signup`, userData);
       return res.data;
 }
 
 
+
+export const userProfile = async (): Promise<UserData | null> => {
+      //axios.defaults.headers.common.Authorization = `Bearer ${authToken}`;
+      const res = await api(3000).get<UserData>(`/auth/profile`);
+      return res.data;
+}
