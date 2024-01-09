4ea53cf9bf5ce95e420121a5eca6b7693edbaff1
Hacene Sadoudi <sadoud_h@etna-alternance.net>
2024-01-09 17:20:12 +0100

    add flipper config to store

--- GPE/fitfrenzy_front/src/redux/store.ts
+++ GPE/fitfrenzy_front/src/redux/store.ts
@@ -5,8 +5,15 @@ import Logger from "redux-logger"
 
 const loggerMiddleware: Middleware = Logger
 
+const middlewares = [
+  /* other middlewares */
+];
+
+
+  const createDebugger = require('redux-flipper').default;
+
 export default configureStore({
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(loggerMiddleware),
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(createDebugger() ),
   devTools: false,
   enhancers: getDefaultEnhancers => getDefaultEnhancers().concat(reactotron.createEnhancer!()),
   reducer: {
