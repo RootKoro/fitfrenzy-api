63e45f659c2a7b03fe34b0ac8fd7698aedb2f80f
Cyr Mathieu <gueye_d@etna-alternance.net>
2024-01-22 15:52:01 +0100

    erasing front from this branch

--- GPE/fitfrenzy_front/src/styles/themes.ts
+++
@@ -1,9 +0,0 @@
-
-export const theme = {
-  BG_COLOR: 'white',
-  BLACK_COLOR: 'rgb(38, 38, 38)',
-  TEXT_COLOR: '#262626',
-  GRAY_COLOR: '#fafafa',
-  BORDER_COLOR: '#dbdbdb',
-  BLUE_COLOR: '#0095f6',
-};
