753ce0f9bbf87839351e2a98f61eb12e2e05903e
Cyr Mathieu <gueye_d@etna-alternance.net>
2024-02-15 01:33:15 +0100

    update

--- GPE/questionary-service/src/questions/question.module.ts
+++ GPE/questionary-service/src/questions/question.module.ts
@@ -3,6 +3,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { QuestionService } from './question.service';
 import { QuestionController } from './question.controller';
 import { Question, QuestionSchema } from 'src/schemas/question.schema';
+import { ResponseService } from './response.service';
+import { ResponseController } from './response.controller';
+import { Response, ResponseSchema } from 'src/schemas/response.schema';
 
 @Module({
   imports: [
@@ -11,9 +14,13 @@ import { Question, QuestionSchema } from 'src/schemas/question.schema';
         name: Question.name,
         schema: QuestionSchema,
       },
+      {
+        name: Response.name,
+        schema: ResponseSchema,
+      }
     ]),
   ],
-  controllers: [QuestionController,],
-  providers: [QuestionService,]
+  controllers: [QuestionController, ResponseController],
+  providers: [QuestionService, ResponseService]
 })
 export class QuestionModule { }
