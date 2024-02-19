b7b85e0299967658a9b4177fd2d342e0a896817e
Cyr Mathieu <gueye_d@etna-alternance.net>
2024-02-19 10:37:00 +0100

    added a new field in the sports model

--- GPE/sport-service/src/sports/dto/create-sport.dto.ts
+++ GPE/sport-service/src/sports/dto/create-sport.dto.ts
@@ -1,36 +1,41 @@
 import {
-    IsNotEmpty,
-    IsString,
-    IsNumber,
-    MaxLength,
-    Min,
-    Max,
-  } from 'class-validator';
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  MaxLength,
+  Min,
+  Max,
+} from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
-  export class CreateSportDto {
-    @ApiProperty({type: String})
-    @IsString()
-    @IsNotEmpty()
-    @MaxLength(300)
-    label: string;
-    
-    @ApiProperty({type: Number})
-    @IsNumber()
-    @IsNotEmpty()
-    @Min(0)
-    @Max(5)
-    rate: number;
-    
-    @ApiProperty({type: String})
-    @IsString()
-    @IsNotEmpty()
-    @MaxLength(300)
-    difficulty: string;
-    
+export class CreateSportDto {
+  @ApiProperty({ type: String })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(300)
+  label: string;
 
-    @ApiProperty({type: String})
-    @IsString()
-    @MaxLength(500)
-    other_information: string
-  }
+  @ApiProperty({ type: String })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(300)
+  category: string;
+
+  @ApiProperty({ type: String })
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(300)
+  difficulty: string;
+
+  @ApiProperty({ type: Number })
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(0)
+  @Max(5)
+  rate: number;
+
+  @ApiProperty({ type: String })
+  @IsString()
+  @MaxLength(500)
+  description: string;
+}
