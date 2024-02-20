60f270407b32aadc0ebe53d64812e4c324d7081a
Hacene Sadoudi <hacene@smartb.city>
2024-02-20 20:25:11 +0100

    add question options to question schema

--- GPE/questionary-service/src/questions/dto/create-question.dto.ts
+++ GPE/questionary-service/src/questions/dto/create-question.dto.ts
@@ -1,17 +1,38 @@
 import {
     IsNotEmpty,
     IsString,
+    IsEnum,
+    ValidateNested
 } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 
-export class CreateQuestionDto {
-  @ApiProperty({type: String})
+enum QuestionType {
+  SINGLE_CHOICE = 'SINGLE_CHOICE',
+  MULTIPLE_CHOICE = 'MULTIPLE_CHOICE',
+}
+
+class Option {
   @IsNotEmpty()
   @IsString()
-  questionary_type: string; // short | long
+  text: string;
+
+  @IsString()
+  icon?: string;
+}
+
+export class CreateQuestionDto {
+  @IsEnum(QuestionType)
+  type: string;
 
   @ApiProperty({type: String})
   @IsNotEmpty()
   @IsString()
   question: string;
+
+  @IsNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => Option)
+  options: Option[];
+
 }
--- GPE/questionary-service/src/schemas/question.schema.ts
+++ GPE/questionary-service/src/schemas/question.schema.ts
@@ -3,15 +3,38 @@ import { Document } from 'mongoose';
 
 export type QuestionDocument = Question & Document;
 
+export enum QuestionType {
+  SINGLE_CHOICE = 'SINGLE_CHOICE',
+  MULTIPLE_CHOICE = 'MULTIPLE_CHOICE',
+}
+
+export class Option {
+  @Prop({ required: true })
+  text: string;
+
+  @Prop({ required: false })
+  icon: string;
+}
+
 @Schema({
   timestamps: { createdAt: 'created', updatedAt: 'updated' },
 })
 export class Question {
-  @Prop({ required: true })
-  questionary_type: string;
+  @Prop({ 
+    required: true,
+    enum: Object.values(QuestionType), 
+    default: QuestionType.SINGLE_CHOICE
+  })
+  type: string;
 
   @Prop({ required: true })
   question: string;
+
+  @Prop({
+    type:[{text:{type:String}, icon:{type:String}}]
+  })
+  options: { quantity: string; product: string }[];
+
 }
 
 export const QuestionSchema = SchemaFactory.createForClass(Question);
